5dc352990b26ebd9a8c40fd294eda77e5730469b
fredsolis17 <152208128+fredsolis17@users.noreply.github.com>
2023-12-07 13:36:17 -0600

    Update main.cpp
    Menu and case a done

--- main.cpp
+++ main.cpp
@@ -6,22 +6,53 @@
 
 using namespace std;
 
-void PrintMenu(const string playlistTitle) {
-    cout << playlistTitle << " PLAYLIST MENU" << endl;   // PLAYLIST MENU SECTION
-    cout << "a - Add song" << endl;
-    cout << "d - Remove song" << endl;
-    cout << "c - Change position of song" << endl;
-    cout << "s - Output songs by specific artist" << endl;
-    cout << "t - Output total time of playlist (in seconds)" << endl;
-    cout << "o - Output full playlist" << endl;
-    cout << "q - Quit" << endl;
-    cout << endl;
+void PrintMenu(const string& playlistTitle) {
+   cout << playlistTitle << " PLAYLIST MENU" << endl;
+   cout << "a - Add song" << endl;
+   cout << "d - Remove song" << endl;
+   cout << "c - Change position of song" << endl;
+   cout << "s - Output songs by specific artist" << endl;
+   cout << "t - Output total time of playlist (in seconds)" << endl;
+   cout << "o - Output full playlist" << endl;
+   cout << "q - Quit" << endl;
+   cout << endl;
 }
 
-PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
-   /* Type your code here */
+PlaylistNode* ExecuteMenu(char option, const string& playlistTitle, PlaylistNode* headNode) {
+   switch (option) {
+      case 'a': {
+         cout << "ADD SONG" << endl;
+         string uniqueID, songName, artistName;
+         int songLength;
    
-}
+         cout << "Enter song's unique ID:" << endl;
+         getline(cin, uniqueID);
+
+         cout << "Enter song's name:" << endl;
+         getline(cin, songName);
+
+         cout << "Enter artist's name:" << endl;
+         getline(cin, artistName);
+
+         cout << "Enter song's length (in seconds):" << endl;
+         cout << endl;
+         cin >> songLength;
+         cin.ignore();
+
+         PlaylistNode* newNode = new PlaylistNode(uniqueID, songName, artistName, songLength);
+         if (headNode == nullptr) {
+            headNode = newNode;
+            } else {
+               PlaylistNode* current = headNode;
+               while (current->GetNext() != nullptr) {
+                  current = current->GetNext();
+               }
+                  
+                current->InsertAfter(newNode);
+            }
+
+            break;
+      }
 
 int main() {
    /* Type your code here */
