f0314f77789e55354d5eb6f386a1c5384c29df6a
fredsolis17 <152208128+fredsolis17@users.noreply.github.com>
2023-12-07 10:58:16 -0600

    Update main.cpp
    zyBooks main.cpp default template - will add playlist menu

--- main.cpp
+++ main.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
+#include "PlaylistNode.h"
 
+//GitHub Repository Link: 
+//Team Members: Fred Solis III & Garrison Regala
+
+using namespace std;
+
+void PrintMenu(const string playlistTitle) {
+   /* Type your code here */
+   
+}
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+   
+   return 0;
+}
