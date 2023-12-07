72a3673e27de9e4b15c78aff28f43a3286ce6926
fredsolis17 <152208128+fredsolis17@users.noreply.github.com>
2023-12-07 14:11:40 -0600

    Update PlaylistNode.cpp
    Last section of PlaylistNode.cpp added with print functions included - PlaylistNode.cpp program done

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -35,3 +35,10 @@ void PlaylistNode::InsertAfter(PlaylistNode* nodePtr) {
    nodePtr->nextNodePtr = nextNodePtr;
    nextNodePtr = nodePtr;
 }
+
+void PlaylistNode::PrintPlaylistNode() const {
+   cout << "Unique ID: " << uniqueID << endl;
+   cout << "Song Name: " << songName << endl;
+   cout << "Artist Name: " << artistName << endl;
+   cout << "Song Length (in seconds): " << songLength << endl;
+}
