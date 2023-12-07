297a3fabcbbdc9d978b7410a88d4a387db61670b
fredsolis17 <152208128+fredsolis17@users.noreply.github.com>
2023-12-07 11:08:42 -0600

    Update main.cpp
    Playlist menu added - implements correctly

--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@
 using namespace std;
 
 void PrintMenu(const string playlistTitle) {
-   /* Type your code here */
-   
+    cout << playlistTitle << " PLAYLIST MENU" << endl;   // PLAYLIST MENU SECTION
+    cout << "a - Add song" << endl;
+    cout << "d - Remove song" << endl;
+    cout << "c - Change position of song" << endl;
+    cout << "s - Output songs by specific artist" << endl;
+    cout << "t - Output total time of playlist (in seconds)" << endl;
+    cout << "o - Output full playlist" << endl;
+    cout << "q - Quit" << endl;
+    cout << endl;
 }
 
 PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
